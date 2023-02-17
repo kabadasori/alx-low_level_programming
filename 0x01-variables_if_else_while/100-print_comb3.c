cacabcf19054ee860be39329ca4665af6e21e2b7
myname <mygmail@gmail.com>
2023-02-17 03:38:02 -0800

    100-print_comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,26 +1,28 @@
 #include <stdio.h>
 
 /**
-  * main - Prints the numbers from 00 to 99
+  * main - Prints the numbers from 01 to 89
   *
   * Return: Always (Success)
   */
 int main(void)
 {
 	int d, p;
-	
+
+
 	for (d = '0'; d < '9'; d++)
 	{
-		for (p = d + 1; p <= '9'; p++)
+		for (p = '0'; p <= '9'; p++)
 		{
-			if (p != d)
-			{
-				putchar(d);
-				putchar(p);
-				if (d == '8' && p == '9')
-				continue;
-				putchar(',');
-				putchar(' ');
+                        if (p != d)
+                        {
+			   putchar(d);
+			   putchar(p);
+
+			if (d == '8' && p == '9')
+			continue;
+			putchar(',');
+			putchar(' ');
 			}
 		}
 	}
