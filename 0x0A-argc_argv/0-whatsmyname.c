19498ef37eb3afea490d4e60aaac9ee9e1f62e6d
myname <mygmail@gmail.com>
2023-03-10 01:45:00 -0800

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,19 +1,16 @@
-/*
- * File: 0-whatsmyname.c
- * Kabada
- */
-
+#include "main.h"
 #include <stdio.h>
 
 /**
- * main - Prints the program name, followed by a new line.
- * @argc: The number of arguments supplied to the program.
- * @argv: An array of pointers to the arguments.
- *
- * Return: Always o.
- */
-int main(int __attribute__((__unused__)) argc, char *argv[])
+  * main - Prints its name
+  * @argc: argument count
+  * @argv: argument vector
+  *
+  * Return: Always zero
+  */
+int main(int argc, char *argv[])
 {
+	(void) argc;
 	printf("%s\n", argv[0]);
 
 	return (0);
