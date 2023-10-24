962f963a03d002412235ab6d31c3117ff474b1fb
kabadasori <38851055+kabadasori@users.noreply.github.com>
2023-10-24 17:29:14 +0300

    Update 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,12 @@
 #!/usr/bin/node
 const fs = require('fs');
 fs.readFile(process.argv[2], 'utf8', function (error, content) {
-  console.log(error || content);
+  if (error){
+    console.error('Error reading file:',error);
+  }
+  else{
+     console.log( content);
+  }
+  
+ 
 });
